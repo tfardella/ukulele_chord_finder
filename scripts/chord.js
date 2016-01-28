f58a9550740db3aa5523cea52413fa3ae145298f
Tony Fardella <tfardella@gmail.com>
2016-01-28 15:17:47 -0800

    changed to use SnapSVG library

--- scripts/chord.js
+++ scripts/chord.js
@@ -4,25 +4,36 @@
 
 var Chord = function()
 {
-	this.chordCode = 00000;
+	this.chordCode = 0;
 	this.chordData = {};
 	this.elem = null;
 	this.paper = null;
 	this.chordList = ukeChords;
 	this.showAlt = false;
+	this.stringLength = 0;
+	this.stringSpacing = 20;
+	this.fretSpacing = 20;
 
-	this.config = {
+	this.config = 
+	{
+		//Default values
 		"editable": false,
-		"chordWidth": 80,
+		"chordWidth": 100,
 		"numStrings": 4,
 		"numFrets": 5
-	}
+	};
+
+// paper height = chordWidth * 1.41666666666667
+
+	this.stringSpacing = ( this.config.chordWidth * 0.6) / ( this.config.numStrings - 1 );
+	this.fretSpacing = this.config.chordWidth * 0.2;
+	this.dotRadius = this.config.chordWidth * 0.04;
 };
 
 Chord.prototype.clear = function( code )
 {
 	this.paper.clear();
-}
+};
 
 Chord.prototype.lookupByCode = function( code )
 {
@@ -37,11 +48,13 @@ Chord.prototype.lookupByCode = function( code )
 	}
 
 	return chords;
-}
+};
 
-Chord.prototype.lookupByName = function( name )
+Chord.prototype.lookupByName = function( name, alt )
 {
 	var list = this.chordList, i = 0, chords = [];
+	
+	alt = ( typeof alt !== "undefined" ) ? true : false;
 
 	for( i = 0; i < list.length; i++ )
 	{
@@ -52,11 +65,13 @@ Chord.prototype.lookupByName = function( name )
 	}
 
 	return chords;
-}
+};
 
-Chord.prototype.lookupByKey = function( key )
+Chord.prototype.lookupByKey = function( key, alt )
 {
 	var list = this.chordList, i = 0, chords = [];
+
+	alt = ( typeof alt !== "undefined" ) ? true : false;
 
 	for( i = 0; i < list.length; i++ )
 	{
@@ -67,11 +82,13 @@ Chord.prototype.lookupByKey = function( key )
 	}
 
 	return chords;
-}
+};
 
 Chord.prototype.drawChord = function( elem, chordData )
 {
 	var self = this;
+	var pathStr;
+
 	self.chordData = chordData;
 	self.elem = elem;
 
@@ -80,14 +97,92 @@ Chord.prototype.drawChord = function( elem, chordData )
 	var chordWidth = self.config.chordWidth;
 	var path = {};
 	var numStrings = self.config.numStrings;
-	var stringWidth = 20;
+	var stringSpacing = self.stringSpacing;
+	var fretSpacing = self.fretSpacing;
 	var numFrets = self.config.numFrets;
 	var editable = self.config.editable;
-	var fretHeight = 20;
 	var ch = null;
 	var el = null;
 	var dotRadius;
+	var dotDiameter;
 	var topPadding = 4;
+	var nutX = null;
+	var nutY = null;
+	var ttText = null;
+	var ttBgRect = null;
+
+	function initTooltip()
+	{
+		ttBgRect = ch.rect( 0, 0, 55, 17, 4, 4 );
+		
+		ttBgRect.attr( {
+				'visibility': 'hidden',
+				'id': 'tooltip_bg'
+			} );
+
+		ttBgRect.addClass( 'tooltip_bg' );
+		
+		ttText = ch.text( 0, 0, "Tooltip" );
+		
+		ttText.attr( {
+				'visibility': 'hidden',
+				'id': 'tooltip'
+			} );
+		
+		ttText.addClass( 'tooltip' );
+	}
+
+	var showTooltip = function( evt, text )
+	{
+	    ttText.node.innerHTML = text;
+
+	    var length = ttText.getBBox().width;
+	    var x = 0, y = 0;
+
+	    ttText.attr( {
+	    	// "x": ( evt.clientX + 11 ),
+	    	// "y": ( evt.clientY + 27 ),
+	    	// 'x': ( evt.offsetX + 11 ),
+	    	// 'y': ( evt.offsetY + 27 ),
+	    	'x': ( evt.offsetX - 20  ),
+	    	'y': ( evt.offsetY + 14 ),
+	    	'fill':'black',
+	    	'visibility':"visible"
+	    } );
+
+	    // TODO: make sure the tooltip is not going past the right edge of the paper
+	    if( evt.offsetX + ( length + 8 ) < chordWidth )
+	    {
+	    	x = evt.offsetX - 24;
+	    }
+	    else
+	    {
+	    	x = chordWidth - ( length + 8 ) - 8;
+	    }
+
+	    ttBgRect.attr( {
+	    	'width': ( length + 8 ),
+	    	// "x": ( evt.clientX + 8 ),
+	    	// "y": ( evt.clientY + 14 ),
+	    	// 'x': ( evt.offsetX + 8 ),
+	    	// 'y': ( evt.offsetY + 14 ),
+	    	'x': x,
+	    	'y': ( evt.offsetY + 14 ),
+	    	'visibility':"visible"
+	    } );
+	}
+
+	var hideTooltip = function( evt )
+	{
+	    ttText.attr( {
+	    	"visibility":"hidden"
+	    } );
+
+	    ttBgRect.attr( {
+	    	"visibility":"hidden"
+	    } );
+
+	}
 
 	function getString( x )
 	{
@@ -95,9 +190,9 @@ Chord.prototype.drawChord = function( elem, chordData )
 
 		for( i = 0; i < numStrings; i++ )
 		{
-			spos = ( i + 1 ) * stringWidth;
+			spos = ( i + 1 ) * stringSpacing;
 
-			if( x > ( spos - 4 ) && x < ( spos + 4 ) )
+			if( x > ( spos - ( dotRadius * 2 ) ) && x < ( spos + ( dotRadius * 2 ) ) )
 			{
 				string = ( i + 1 );
 			}
@@ -112,11 +207,11 @@ Chord.prototype.drawChord = function( elem, chordData )
 
 		for( i = 0; i < numFrets; i++ )
 		{
-			fstart = nutY + ( i * fretHeight );
+			fstart = nutY + ( i * fretSpacing );
 
-			if( y > fstart && y < ( fstart + fretHeight ) )
+			if( y > fstart && y < ( fstart + fretSpacing ) )
 			{
-				fret = ( i + 1 );
+				fret = i + 1 ;
 			}
 		}
 
@@ -166,10 +261,10 @@ Chord.prototype.drawChord = function( elem, chordData )
 		}
 	}
 
-	if( chordData.alt === true && self.showAlt === false )
-	{
-		return;
-	}
+	// if( chordData.alt === true && self.showAlt === false )
+	// {
+	// 	return;
+	// }
 	
 	if( typeof elem === "string" )
 	{
@@ -182,7 +277,8 @@ Chord.prototype.drawChord = function( elem, chordData )
 
 	if( self.paper === null )
 	{
-		ch = Raphael( el, chordWidth, ( chordWidth * 1.6 ) );
+//		ch = Snap( el, chordWidth, ( chordWidth * 1.6 ) );
+		ch = Snap( el );
 		self.paper = ch;
 	}
 	else
@@ -191,70 +287,96 @@ Chord.prototype.drawChord = function( elem, chordData )
 	}
 
 	
-	fretHeight = chordWidth * 0.2;
+	fretSpacing = self.fretSpacing = chordWidth * 0.2;
 	dotRadius = chordWidth * 0.04;
+	dotDiameter = dotRadius * 2;
 
 	var filledDot = function( str, fret )
 	{
-		var x = ( str - 1 ) * stringWidth + nutX;
-		var y = fret * fretHeight - ( fretHeight / 2 )  + nutY;
+		var x = ( str - 1 ) * stringSpacing + nutX;
+		var y = fret * fretSpacing - ( fretSpacing / 2 )  + nutY;
 
 		var circ = ch.circle( x, y, dotRadius + 2 )
 			.attr( { "fill": "#000000", "stroke": "none" } );
 
-		if( editable === true )
+		if( editable )
 		{
+			circ.addClass( "clickable" );
+
 			circ.node.onclick = function( evt )
 				{	
-					var s = getString( evt.layerX );
+					var s = getString( evt.offsetX );
 					setData( s, "0" );
 
 					ch.clear();
 					self.drawChord( elem, chordData );
-				}
+				};
 		}
 	};
 
 	var openDot = function( str )
 	{
 		var radius = dotRadius;
-		var x = ( str - 1 ) * stringWidth + nutX;
+		var x = ( str - 1 ) * stringSpacing + nutX;
 		var y = nutY - ( radius * 2 );
 		var circ = ch.circle( x, y, radius )
 			.attr( { "fill": "white", "stroke": "#444444", "stroke-width": 1 } );
 
-		if( editable === true )
+		if( editable )
 		{
+			circ.addClass( "clickable" );
+
 			circ.node.onclick = function( evt )
 				{
-					var s = getString( evt.layerX );
+					var s = getString( evt.offsetX );
 					setData( s, -1 );
 
 					ch.clear();
 					self.drawChord( elem, chordData );
 //					console.log( "OpenDot clicked - layerX: " + evt.layerX + " - layerY: " + evt.layerY );
-				}
+				};
+
+			circ.node.onmousemove = function( evt )
+			{
+				showTooltip( evt, "Mute string" );
+			}
+			circ.node.onmouseout = function( evt )
+			{
+				hideTooltip( evt );
+			}
 		}
 	};
 	
 	var muteString = function( str )
 	{
 		var radius = dotRadius;
-		var tx = ( str - 1 ) * stringWidth + nutX;
-		var ty = nutY - ( radius * 2.4 );
+		var tx = ( str - 1 ) * stringSpacing + ( nutX * 0.8) ;
+		var ty = nutY - ( radius * 1.2 );
 		var txt = ch.text( tx, ty, "x" )
 			.attr( { "font-size": 14, fill: "#444444" } );
 
-		if( editable === true )
+		if( editable )
 		{
+			txt.addClass( "clickable" );
+			txt.attr( { "title": "Click to unmute string" } );
+
 			txt.node.onclick = function( evt )
 				{
-					var s = getString( evt.layerX );
+					var s = getString( evt.layerX - stringSpacing );
 					setData( s, 0 );
 					ch.clear();
 					self.drawChord( elem, chordData );
+
 //					console.log( "muteString clicked - layerX: " + evt.layerX + " - layerY: " + evt.layerY );
-				}
+				};
+			txt.node.onmousemove = function( evt )
+			{
+				showTooltip( evt, "Unmute string" );
+			}
+			txt.node.onmouseout = function( evt )
+			{
+				hideTooltip( evt );
+			}
 		}
 	};
 
@@ -262,7 +384,8 @@ Chord.prototype.drawChord = function( elem, chordData )
 
 	if( editable )
 	{
-		var cl = self.lookupByCode( chordData.code ), i = 0;
+		var cl = self.lookupByCode( chordData.code );
+
 		if( cl.length === 0 )
 		{
 			chordData.name = "????";
@@ -270,32 +393,34 @@ Chord.prototype.drawChord = function( elem, chordData )
 		else
 		{
 			chordData.name = "";
+
 			for( i = 0; i < cl.length; i++ )
 			{
 				chordData.name += cl[ i ].name;
 
 				if( i < cl.length - 1 )
 				{
-					chordData.name += " / "
+					chordData.name += " / ";
 				}
 			}
 		}
 	}
 
-	ch.text( ( chordWidth * 0.5 ), ( topPadding * 3 ), chordData.name )
-		.attr( { "font-size": 14, fill: "#444444" } );
+	ch.text( ( chordWidth * 0.5 ), ( topPadding * 4 ), chordData.name )
+		.attr( { "font-size": 14, fill: "#444444", "text-anchor": "middle" } );
 
 	// Draw nut
 	var nutWidth = chordWidth * 0.6; //60;
 	var nl = ( chordWidth - nutWidth ) / 2;
 	var nr = nl + nutWidth;
-	var nut = ch.path( [ "M", nl, ( topPadding + ( chordWidth * 0.38 ) ), "L", nr, ( topPadding +  ( chordWidth * 0.38 ) ) ] )
+	pathStr = "M" + nl + " " + ( topPadding + ( chordWidth * 0.28 ) ) + " " + "L" + nr + " " + ( topPadding +  ( chordWidth * 0.28 ) ) ;
+	var nut = ch.path( pathStr )
 		.attr({ "stroke": "#444444" } );
 
 	// If the chord does not start at the 1st fret change the nut and draw the fret #
-	if( editable === true )
+	if( editable )
 	{
-		if( chordData.code[ 0 ] === "0" )
+		if( chordData.code[ 0 ] === "1" )
 		{
 			nut.attr( { "stroke-width": 5 } );
 		}
@@ -305,9 +430,11 @@ Chord.prototype.drawChord = function( elem, chordData )
 		}
 
 		var tx = ( nut.getBBox().x - ( chordWidth * 0.135) );
-		var ty = ( nut.getBBox().y + ( chordWidth * 0.12 ) );
+		var ty = ( nut.getBBox().y + ( chordWidth * 0.15 ) );
 		var ftxt = ch.text( tx, ty, chordData.code[ 0 ] )
-			.attr( { "font-size": 14, fill: "#444444" } );
+			.attr( { "font-size": 14, fill: "#444444" } )
+			.addClass( "clickable" );
+	
 		ftxt.node.onclick = function( evt )
 			{
 				var s = getString( evt.layerX );
@@ -318,62 +445,74 @@ Chord.prototype.drawChord = function( elem, chordData )
 				}
 				else
 				{
-					f = 0;
+					f = 1;
 				}
 				setData( 0, f );
 				ch.clear();
 				self.drawChord( elem, chordData );
+
 //				console.log( "startFret clicked - layerX: " + evt.layerX + " - layerY: " + evt.layerY );
-			}
+			};
 	}
 	else
 	{
-		if( chordData.code[ 0 ] === "0" )
+		if( chordData.code[ 0 ] === "1" )
 		{
 			nut.attr( { "stroke-width": 5 } );
 		}
 		else
 		{
 			nut.attr( { "stroke-width": 2 } );
-			var tx = ( nut.getBBox().x - ( chordWidth * 0.135) );
-			var ty = ( nut.getBBox().y + ( chordWidth * 0.12 ) );
-			ch.text( tx, ty, chordData.code[ 0 ] )
+			var tx2 = ( nut.getBBox().x - ( chordWidth * 0.17 ) );
+			var ty2 = ( nut.getBBox().y + ( chordWidth * 0.17 ) );
+			var pos = chordData.code[ 0 ];
+			ch.text( tx2, ty2,  pos )
 				.attr( { "font-size": 14, fill: "#444444" } );
 		}
 	}
 
-	var nutX = nut.getBBox().x;
-	var nutY = nut.getBBox().y;
+	nutX = nut.getBBox().x;
+	nutY = nut.getBBox().y;
 
 	// Draw strings
-	var stringLen = ( ( fretHeight * numFrets ) + nutY );
-	stringWidth = nutWidth / ( numStrings - 1 );
+	var stringLen = ( ( fretSpacing * numFrets ) + nutY );
+	stringSpacing = self.stringWidth = nutWidth / ( numStrings - 1 );
+
+//	console.log( "stringSpacing: " + stringSpacing + " nutWidth: " + nutWidth + " chordWidth: " + chordWidth );
+
+	var clickHandler = function( evt )
+		{
+			var s = getString( evt.offsetX );
+			var f = getFret( evt.offsetY );
+			setData( s, f );
+
+//			console.log( "String clicked - layerX: " + evt.layerX + " - layerY: " + evt.layerY + " string: " + s + " fret: " + f );
+			
+			ch.clear();
+			self.drawChord( elem, chordData );
+		};
 
 	for( i = 0; i < numStrings; i++ )
 	{
-		var str = ch.path( [ "M", ( nutX + ( stringWidth * i ) ), nutY, "L", ( nutX + ( stringWidth * i ) ), stringLen ] )
+		pathStr = "M" + ( nutX + ( stringSpacing * i ) ) + " " + nutY + "L" + ( nutX + ( stringSpacing * i ) ) + " " + stringLen;
+		var str = ch.path( pathStr )
+			.addClass( "string" )
 			.attr( { "stroke": "#444444", "stroke-width": 2 } );
 
-		if( editable === true )
+		if( editable )
 		{
-			str.node.onclick = function( evt )
-			{
-				var s = getString( evt.layerX );
-				var f = getFret( evt.layerY );
-				setData( s, f );
+			str.addClass( "clickable" );
 
-//				console.log( "String clicked - layerX: " + evt.layerX + " - layerY: " + evt.layerY + " string: " + s + " fret: " + f );
-				ch.clear();
-				self.drawChord( elem, chordData );
-			}
+			str.node.onclick = clickHandler;
 		}
 	}
 
 	// Draw frets
-	for( i = 1; i < numFrets + 1; i++ )
+	for( i = 1; i < ( numFrets + 1 ); i++ )
 	{
-		var fw = ( ( numStrings - 1 ) * stringWidth ) + nutX;
-		ch.path( [ "M", nutX, ( nutY + ( fretHeight * i ) ), "L", fw, ( nutY + ( fretHeight * i ) ) ] )
+		var fw = ( ( numStrings - 1 ) * stringSpacing ) + nutX;
+		pathStr = "M " + nutX + " " + ( nutY + ( fretSpacing * i ) ) + " L " + fw + " " + ( nutY + ( fretSpacing * i ) );
+		ch.path( pathStr )
 			.attr( { "stroke": "#888888", "stroke-width": 1 } );
 	}
 
@@ -392,6 +531,11 @@ Chord.prototype.drawChord = function( elem, chordData )
 		{
 			filledDot( i , chordData.code[ i ] );
 		}
+	}
+
+	if( editable )
+	{
+//		initTooltip();
 	}
 
 	return el;
--- scripts/main.js
+++ scripts/main.js
@@ -1,53 +1,60 @@
 /*******************************
  * Let's draw some uke chords!!
  */
+$( function() {
+	var chordEd = new Chord();
+	var clist = [];
 
-var chordEd = new Chord();
-var clist = [];
+	// Draw editable chord
+	chordEd.config.chordWidth = 120;
+	chordEd.config.numStrings = 4;
+	chordEd.config.editable = true;
+	chordEd.drawChord( "chordEdit", { "name": null, "code":"10000" } );
 
-chordEd.config.chordWidth = 120;
-chordEd.config.numStrings = 4;
-chordEd.config.editable = true;
-chordEd.drawChord( "chordEdit", { "name": null, "code":"00000" } );
-
-var i = 0;
-
-
-function getChordsByKey( key )
-{
-	var ch = new Chord();
-	return ch.lookupByKey( key );
-}
-
-function drawChords( el, list )
-{
 	var i = 0;
 
-	for( i = 0; i < list.length; i++ )
-	{
-		var chord = new Chord();
-		var id = "chord" + Date.now();
-		var cdiv = $("<div/>").addClass("chord").attr( "id", id );
-		$( el ).append( cdiv );
-		chord.drawChord( id, list[ i ] );
-	}
-}
 
-clist = ukeChords;
-drawChords( "#All", clist );
-
-// Handle tab selection event
-$('#chordTabs a').on('shown.bs.tab', function (e) {
-	var key = e.target.hash.substring( 1 );
-	
-	if( key === "All" )
+	function getChordsByKey( key )
 	{
-		clist = ukeChords;
-	}
-	else
-	{
-		clist = getChordsByKey( key );
+		var ch = new Chord();
+		return ch.lookupByKey( key );
 	}
 
-	drawChords( e.target.hash, clist ); 	
+	function drawChords( el, list )
+	{
+		var i = 0;
+
+		for( i = 0; i < list.length; i++ )
+		{
+			var chord = new Chord();
+			chord.config.chordWidth = 80;
+			chord.config.numStrings = 4;
+			chord.config.editable = false;
+
+			var id = "chord" + Date.now();
+			var csvg = $("<svg version='1.1' xmlns='http://www.w3.org/2000/svg' class='chord' />").attr( "id", id );
+			$( el ).append( csvg );
+			chord.drawChord( id, list[ i ] );
+		}
+	}
+
+	// Fill in the All tab on initial page load
+	clist = ukeChords;
+	drawChords( "#All", clist );
+
+	// Handle tab selection event
+	$('#chordTabs a').on('shown.bs.tab', function (e) {
+		var key = e.target.hash.substring( 1 );
+		
+		if( key === "All" )
+		{
+			clist = ukeChords;
+		}
+		else
+		{
+			clist = getChordsByKey( key );
+		}
+		var el = '#' + key;
+		drawChords( el, clist );
+	});
 });
